b914ab6f8771ec83b57e1bf4415fb31e3766b536
Charles Fries <charliefries@icloud.com>
2021-08-16 15:59:32 -0700

    Add query override

--- addon/adapters/application.js
+++ addon/adapters/application.js
@@ -1,3 +1,59 @@
-import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import CloudFirestoreAdapter from 'ember-cloud-firestore-adapter/adapters/cloud-firestore';
+import RSVP from 'rsvp';
 
-export default class ApplicationAdapter extends JSONAPIAdapter {}
+export default class MyCloudFirestoreAdapter extends CloudFirestoreAdapter {
+	/**
+	 * @override
+	 */
+	query(_store, type, query, recordArray) {
+		return new RSVP.Promise((resolve, reject) => {
+			const collectionRef = this.buildCollectionRef(type.modelName, query);
+			let firestoreQuery = query.filter?.(collectionRef) || collectionRef;
+
+			const { limit, startAt, endAt } = firestoreQuery._delegate._query;
+			if (limit) {
+				firestoreQuery = firestoreQuery.limit(limit + 1);
+			}
+
+			const unsubscribe = firestoreQuery.onSnapshot(
+				async (querySnapshot) => {
+					if (query.isRealtime && !this.isFastBoot) {
+						this.realtimeTracker.trackQueryChanges(firestoreQuery, recordArray, query.queryId);
+					}
+
+					const meta = {
+						canNext: false,
+						canPrevious: false,
+					};
+
+					let { docs } = querySnapshot;
+					if (limit) {
+						if (startAt) {
+							meta.canNext = docs.length > limit;
+							meta.canPrevious = true;
+							docs = docs.slice(0, limit);
+						} else if (endAt) {
+							meta.canNext = true;
+							meta.canPrevious = docs.length > limit;
+							docs = docs.length === limit ? docs : docs.slice(1).slice(-limit);
+						} else {
+							meta.canNext = docs.length > limit;
+							meta.canPrevious = false;
+							docs = docs.slice(0, limit);
+						}
+					}
+
+					resolve({
+						docs: docs.map((docSnapshot) => {
+							return Object.assign(docSnapshot.data(), { id: docSnapshot.id });
+						}),
+						meta,
+					});
+
+					unsubscribe();
+				},
+				(error) => reject(error)
+			);
+		});
+	}
+}
